e1b49b0c0243bb03b0c7e44b0046ff72ba54ee33
Elena Semenova <leo110274@yandex.ru>
2023-02-19 20:18:04 +0300

    add Task1, Task2

---
+++ Task1/Program.cs
@@ -0,0 +1,70 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+//Console.Write("Введите по очереди любое количество чисел одной строкой через пробел: ");
+//int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int GetNumber(string message)
+{
+int result = 0;
+while (true)
+{
+    Console.WriteLine(message);
+    if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число! Повтороите ввод");
+    } 
+}
+return result;
+}
+
+int [] InitArray(int demensions)
+{
+    int [] result = new int[demensions];
+ //   Console.WriteLine("Введите количество чисел, которые планируете ввести");
+   // demensions = Convert.ToInt32(Console.ReadLine()??"");
+    for (int i = 0; i < demensions; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        result[i] = Convert.ToInt32(Console.ReadLine()??"");
+    }
+
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+
+    Console.WriteLine();
+}
+
+int CheckPositiveNumber(int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+//Console.WriteLine("Введите количество чисел, которые планируете ввести");
+///int demensions = Convert.ToInt32(Console.ReadLine()??"");
+int demensions = GetNumber("Введите количество чисел, которые планируете ввести");
+int[] arr = InitArray(demensions);
+PrintArray(arr);
+int countPositiveNumber = CheckPositiveNumber(arr);
+Console.WriteLine($"Количество чисел больше нуля = {countPositiveNumber}");
---
+++ Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int GetNumber(string message)
+{
+int result = 0;
+while (true)
+{
+    Console.WriteLine(message);
+    if (int.TryParse(Console.ReadLine(), out result))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное число! Повтороите ввод");
+    } 
+}
+return result;
+}
+
+void FindIntersectionPoint(int b1, int k1, int b2, int k2)
+{
+    if (k1 != k2 && b1 != b2)
+    {
+    double x = (double) (b2 - b1)/(k1 - k2);
+    double y = (double) k1*(b2-b1)/(k1-k2) + b1;
+    Console.WriteLine($"Точка пересечения двух прямых ({x} ; {y})");
+    Console.WriteLine($"Х: {x:0.00}");
+    Console.WriteLine($"Y: {y:0.00}");
+    }
+    
+    else Console.WriteLine("Прямые параллельны или сопадают");
+}
+
+int b1 = GetNumber("Введите координату Х первой точки");
+int k1 = GetNumber("Введите координату Y первой точки");
+int b2 = GetNumber("Введите координату Х второй точки");
+int k2 = GetNumber("Введите координату Н второй точки");
+Console.Write($"Координаты первой точки:({b1},{k1}) координаты второй точки:({b2},{k2})");
+Console.WriteLine();
+FindIntersectionPoint (b1, k1, b2, k2);
+
